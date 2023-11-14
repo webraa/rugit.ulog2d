dc91d30d7801289d091650b670518532423e9276
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-11-14 18:29:30 +0300

    Update re-create-mainlog.sh

--- scripts/re-create-mainlog.sh
+++ scripts/re-create-mainlog.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-path=$(pwd)
-
 sudo rm -vf /var/log/ulog/mainlog-sqlt3.db
-sudo sqlite3 /var/log/ulog/mainlog-sqlt3.db < $path/sqlt3-schm/mainlog-table.schema
+sudo sqlite3 /var/log/ulog/mainlog-sqlt3.db < ./sqlt3-schm/mainlog-table.schema
