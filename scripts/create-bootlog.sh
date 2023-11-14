7afafecb8a3971eaa52fafa02bc5c4a6cfc2e3c9
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-11-14 17:52:52 +0300

    Update create-bootlog.sh

--- scripts/create-bootlog.sh
+++ scripts/create-bootlog.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-
-echo 'lets.. do it!!'
-
-sudo sqlite3 /var/log/ulog/main-sqlt3.db < ./sqlt3-schm/field-set.schema
-sudo sqlite3 /var/log/ulog/bootlog-sqlt3.db < ./sqlt3-schm/field-set.schema
+sudo sqlite3 /var/log/ulog/bootlog-sqlt3.db < ./sqlt3-schm/bootlog-table.schema
