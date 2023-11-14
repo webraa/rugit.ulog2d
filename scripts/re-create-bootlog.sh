b68553bea92724e9660c0e00042f1c4318435164
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-11-14 18:26:45 +0300

    Update re-create-bootlog.sh

--- scripts/re-create-bootlog.sh
+++ scripts/re-create-bootlog.sh
@@ -4,4 +4,4 @@ path=$(pwd)
 
 sudo rm -vf /var/log/ulog/bootlog-sqlt3.db
 sudo cat $path/sqlt3-schm/bootlog-table.schema
-sudo sqlite3 /var/log/ulog/bootlog-sqlt3.db < ./sqlt3-schm/bootlog-table.schema
+sudo sqlite3 /var/log/ulog/bootlog-sqlt3.db < $path/sqlt3-schm/bootlog-table.schema
